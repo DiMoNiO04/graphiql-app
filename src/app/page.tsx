c27f6e1b07a10525a222b2889b5ea199bc7d4f91
DiMoNiO04 <dima.razumov.940@mail.ru>
2024-08-23 17:22:25 +0300

    init: delete DS_STORE duplicate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,5 @@
-'use client';
-
-import Button from '@mui/material/Button';
 import React from 'react';
 
 export default function Home() {
-  return (
-    <main>
-      <Button variant="outlined" onClick={() => console.log('1')}>
-        Contained
-      </Button>
-      Start project
-    </main>
-  );
+  return <main>Start project</main>;
 }
