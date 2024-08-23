a61be65d9b03c3967d95622998cf976158767210
DiMoNiO04 <dima.razumov.940@mail.ru>
2024-08-23 16:21:20 +0300

    init: instal material-ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
+'use client';
+
+import Button from '@mui/material/Button';
 import React from 'react';
 
 export default function Home() {
-  return <main>Start project</main>;
+  return (
+    <main>
+      <Button variant="outlined" onClick={() => console.log('1')}>
+        Contained
+      </Button>
+      Start project
+    </main>
+  );
 }
