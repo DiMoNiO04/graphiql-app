e5669b9bf61b1de8000a625419b268cbc4588dc1
Yuliya Shelenhouskaya <yulchik241@mail.ru>
2024-08-26 01:46:45 +0300

    feat: add footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,29 @@
+import Stack from '@mui/material/Stack';
+import RSLogo from './RSSchool-Logo/RSLogo';
+import Box from '@mui/material/Box';
+import GithubLinks from './Github-links/GithubLinks';
+
 const Footer = () => {
-  return <footer>© 2024</footer>;
+  return (
+    <footer>
+      <Stack
+        direction="row"
+        sx={{
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          padding: '1.5rem',
+          boxShadow:
+            '0px 2px 4px -1px rgba(0, 0, 0, 0.3), 0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12)',
+        }}
+      >
+        <GithubLinks />
+        <p>© 2024</p>
+        <Box sx={{ width: 50, height: 50 }}>
+          <RSLogo />
+        </Box>
+      </Stack>
+    </footer>
+  );
 };
 
 export default Footer;
---
+++ src/constants/constants.ts
@@ -0,0 +1,17 @@
+export const developers = [
+  {
+    href: 'https://github.com/Yuliya241',
+    imgSrc: 'https://avatars.githubusercontent.com/u/119517257?v=4',
+    imgAlt: 'Yuliya',
+  },
+  {
+    href: 'https://github.com/DiMoNiO04',
+    imgSrc: 'https://avatars.githubusercontent.com/u/91553443?v=4',
+    imgAlt: 'Dmitry',
+  },
+  {
+    href: 'https://github.com/tarasovcad',
+    imgSrc: 'https://avatars.githubusercontent.com/u/157248212?v=4',
+    imgAlt: 'Maksim',
+  },
+];
