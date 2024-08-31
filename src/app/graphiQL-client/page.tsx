e8a9de71e468977489a76efe798d638921d6e068
DiMoNiO04 <dima.razumov.940@mail.ru>
2024-08-31 20:14:42 +0300

    feat: add UrlEditorGraphi component

--- src/app/graphiQL-client/page.tsx
+++ src/app/graphiQL-client/page.tsx
@@ -1,5 +1,36 @@
+'use client';
+
+import UrlEditorGraphi from '@/src/components/UrlEditorGraphi/UrlEditorGraphi';
+import { Box, Stack, Typography } from '@mui/material';
+import { useState } from 'react';
+
 const GraphiQlClient = () => {
-  return <h1>GraphiQL Client</h1>;
+  const [endpointUrl, setEndpointUrl] = useState<string>('');
+  const [sdlUrl, setSdlUrl] = useState<string>('');
+
+  return (
+    <Stack
+      spacing={5}
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        padding: '4rem 2.5rem',
+      }}
+    >
+      <Typography variant="h4" component="h1" sx={{ textAlign: 'center' }}>
+        GraphiQL Client
+      </Typography>
+      <Box>
+        <UrlEditorGraphi
+          endpointUrl={endpointUrl}
+          onEndpointUrlChange={setEndpointUrl}
+          sdlUrl={sdlUrl}
+          onSdlUrlChange={setSdlUrl}
+        />
+      </Box>
+    </Stack>
+  );
 };
 
 export default GraphiQlClient;
