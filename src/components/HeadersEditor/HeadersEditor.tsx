92573a79e2d7fd9ab55ba0eca5530bc846602cb9
DiMoNiO04 <dima.razumov.940@mail.ru>
2024-09-01 12:04:12 +0300

    refactor: headers editor component

--- src/components/HeadersEditor/HeadersEditor.tsx
+++ src/components/HeadersEditor/HeadersEditor.tsx
@@ -1,6 +1,8 @@
 import { IHeadersEditor } from '@/src/types/headersEditorTypes';
-import { Box, Button, Stack, TextField } from '@mui/material';
+import { Box, Button, Stack, TextField, Typography } from '@mui/material';
 import React from 'react';
+import AddIcon from '@mui/icons-material/Add';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 const HeadersEditor: React.FC<IHeadersEditor> = ({ headers, onChange }) => {
   const updateHeader = (index: number, key: string, value: string) => {
@@ -13,40 +15,72 @@ const HeadersEditor: React.FC<IHeadersEditor> = ({ headers, onChange }) => {
     onChange([...headers, { key: '', value: '' }]);
   };
 
+  const removeHeader = (index: number) => {
+    const newHeaders = headers.filter((_, i) => i !== index);
+    onChange(newHeaders);
+  };
+
   return (
     <Box>
       <Stack direction="row" spacing={2} alignItems="end">
-        <Stack direction="column" spacing={2} alignItems="center">
+        <Stack direction="column" spacing={2} alignItems="end">
           {headers.map((header, index) => (
-            <Box key={index} sx={{ display: 'flex', mb: 2 }}>
+            <Box key={index} sx={{ display: 'flex', alignItems: 'center', mb: 2 }}>
               <TextField
                 label="Key"
                 value={header.key}
                 onChange={(e) => updateHeader(index, e.target.value, header.value)}
+                variant="outlined"
+                size="small"
+                fullWidth
                 sx={{
-                  height: 40,
                   mr: 2,
-                  '.MuiInputBase-root': {
-                    height: 40,
-                  },
                 }}
               />
               <TextField
                 label="Value"
                 value={header.value}
                 onChange={(e) => updateHeader(index, header.key, e.target.value)}
+                variant="outlined"
+                size="small"
+                fullWidth
                 sx={{
-                  height: 40,
-                  '.MuiInputBase-root': {
-                    height: 40,
-                  },
+                  mr: 2,
+                }}
+              />
+              <DeleteIcon
+                color="error"
+                onClick={() => removeHeader(index)}
+                sx={{
+                  mr: 3,
+                  '&:hover': { cursor: 'pointer', opacity: 0.6 },
                 }}
               />
             </Box>
           ))}
         </Stack>
-        <Button onClick={addHeader} variant="contained" sx={{ mt: 2, backgroundColor: '#000000', color: '#ffffff' }}>
-          Add Header
+        <Button
+          variant="contained"
+          onClick={addHeader}
+          sx={{
+            transition: 'all 0.4s ease',
+            backgroundColor: '#000000',
+            color: '#ffffff',
+            margin: '1.5rem 0',
+            display: 'flex',
+            flexShrink: 0,
+            width: 175,
+            alignSelf: 'flex-end',
+            '&:hover': {
+              color: '#000000',
+              backgroundColor: '#ffffff',
+            },
+          }}
+        >
+          <Box sx={{ alignItems: 'center', display: 'flex' }}>
+            <AddIcon />
+            <Typography marginLeft={1}>Add header</Typography>
+          </Box>
         </Button>
       </Stack>
     </Box>
